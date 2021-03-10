066945c2770405d81977ee12eb9ddf62f95c9add
Tomasz Gietek <tomasz.gietek@omegacode.pl>
2021-03-10 10:03:17 +0100

    v0.1.4

--- init.rb
+++ init.rb
@@ -1,8 +1,10 @@
 require 'redmine'
 require 'searchable_selectbox/hook_listener'
 
-Redmine::Plugin.register :redmine_searchable_selectbox do
-  name 'Redmine Searchable Selectbox'
+Redmine::Plugin.register :redmine_better_selectbox do
+  name 'Redmine Better Selectbox'
   description "This plugin changes Redmine's selectbox searchable."
-  version '0.1.3'
+  version '0.1.4'
+  author 'Tomasz Gietek for Omega Code Sp. z o.o. forked from the original author: farend'
+  author_url 'https://github.com/omegacodepl/redmine_better_selectbox'
 end
